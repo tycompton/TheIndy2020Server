177d4df8be25cf5fdb3dcb6ef692b541bf18a673
tycompton <tyrone.compton@gmail.com>
2020-05-19 00:00:40 +0100

    friendly login validation error messages created

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
 
 require('dotenv').config()
 
@@ -26,9 +27,10 @@ mongoose
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(expressValidator());
   
 // Routes Middleware
-app.use("/api", userRoutes)
+app.use("/api", userRoutes);
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
-const express = require('express'); 
+const express = require("express");
 const router = express.Router();
 
-const { signup } = require('../controllers/user');
+const { signup } = require("../controllers/user");
+const { userSignUpValidator } = require("../validator");
 
-router.post('/signup', signup);
+router.post("/signup", userSignUpValidator, signup);
 
 module.exports = router;
