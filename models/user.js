2e575ebd0e1c8e7d0e752a65e60812e15c1f985c
tycompton <tyrone.compton@gmail.com>
2020-05-18 23:29:31 +0100

    signup tested

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const uuid = require('uuid/v1');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require('uuid');
 
 const userSchema = new mongoose.Schema({
   name: {
