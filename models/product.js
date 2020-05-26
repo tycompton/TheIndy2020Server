d9083b6de66b3acb475813ca85349f26a84c9315
tycompton <tyrone.compton@gmail.com>
2020-05-26 20:49:41 +0100

    Product and categories created

--- models/product.js
+++ models/product.js
@@ -25,6 +25,10 @@ const productSchema = new mongoose.Schema({
   quantity: {
     type: Number,
   },
+  sold:{
+    type: Number,
+    default: 0
+  },
   image: {
     data: Buffer,
     contentType: String
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const router = express.Router();
 
-const { create } = require("../controllers/product");
+const { 
+  create, 
+  productById, 
+  read, 
+  remove, 
+  update, 
+  list, 
+  listRelated, 
+  listCategories, 
+  listBySearch,
+  image 
+} = require("../controllers/product");
+
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
-
+          
+router.get('/product/:productId', read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove); 
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
+
+router.get('/products', list);
+router.get('/products/related/:productId', listRelated);
+router.get('/products/categories', listCategories )
+router.post("/products/by/search", listBySearch);
+router.get('/product/image/:productId', image);
 
 router.param("userId", userById);
+router.param("productId", productById);
 
 module.exports = router;
