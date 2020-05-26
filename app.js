61f3b3318790e49293fdf9bc3333747c42ae97bb
tycompton <tyrone.compton@gmail.com>
2020-05-26 22:05:43 +0100

    app.js edit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 
 require('dotenv').config()
@@ -30,18 +31,13 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
-
-
-// Routes Middleware
+// Middleware Routes
 app.use("/api", authRoutes);
 app.use("/api", userRoutes);
 app.use("/api", categoryRoutes);
 app.use("/api", productRoutes);
-
-
-
-
 
 
 
